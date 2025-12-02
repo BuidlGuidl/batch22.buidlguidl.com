43406aacd4be18e279e63ac8f76806f97d392ceb
Zacheus <segunzacheusi@gmail.com>
2025-12-02 01:51:04 +0100

    feat: created a personal builders page

---
+++ packages/nextjs/app/builders/0x37cFF24EAe0629FE7060EB7D260Ab37492D22E1D/components/InfoCard.tsx
@@ -0,0 +1,8 @@
+const InfoCard = ({ title, children }: { title: string; children: React.ReactNode }) => (
+  <div className="bg-base-100 dark:bg-base-300 rounded-2xl p-8 border border-base-300 dark:border-base-100 shadow-lg dark:shadow-none">
+    <h2 className="text-2xl font-semibold mb-4">{title}</h2>
+    {children}
+  </div>
+);
+
+export default InfoCard;
