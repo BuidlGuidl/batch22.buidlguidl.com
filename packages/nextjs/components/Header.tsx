f90d8e79b263595f1ef69b812946f42e1b97c327
Zacheus <segunzacheusi@gmail.com>
2025-12-07 18:10:41 +0100

    Chore: updated metadata for improved social branding

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -86,8 +86,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Batch 22</span>
+            <span className="text-xs">Building web3 together</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
